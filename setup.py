db908daf4cade04c2698a4aa14bbb991c81f2f65
Heryandi <heryandi@gmail.com>
2013-07-12 22:38:39 +0800

    Remove the scripts listing as it causes error if bin/ is empty

--- setup.py
+++ setup.py
@@ -2,15 +2,13 @@ import os
 
 from setuptools import setup, find_packages
 
-scripts = [os.path.join('bin', filename) for filename in os.listdir('bin')]
-
 setup(
     name='mnp',
     version='1.0.0',
     author='Heryandi',
     author_email='heryandi@gmail.com',
     packages=find_packages(exclude='test'),
-    scripts=scripts,
+    scripts=[],
     url='https://github.com/heryandi/mnp',
     license='MIT',
     description='Wrapper tools to manage Mininet package',
