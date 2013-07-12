90c2141ccdcb566149a0f11e8cc1e3f67b2dc113
Heryandi <heryandi@gmail.com>
2013-07-12 22:39:35 +0800

    Change to use --extra-index-url instead

--- mnp/commands.py
+++ mnp/commands.py
@@ -2,7 +2,7 @@ import subprocess
 
 def download(packages, index_url, additional_args = None):
     additional_args = [] if additional_args is None else additional_args
-    subprocess.check_call(["pip", "install"] + packages + ["-i", index_url] + additional_args)
+    subprocess.check_call(["pip", "install"] + packages + ["--extra-index-url", index_url] + additional_args)
 
 def upload(repository, additional_args = None):
     additional_args = [] if additional_args is None else additional_args
