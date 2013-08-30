5fc1ca4db841b237fdc93f623dcc2a61b578ae89
Heryandi <heryandi@gmail.com>
2013-08-30 19:31:28 +0800

    Add test cases for docs. Refactor test cases a bit.

--- mnp/test/test_command.py
+++ mnp/test/test_command.py
@@ -58,17 +58,35 @@ class XmlRpcTest(unittest.TestCase):
         patcher1 = patch("xmlrpclib.ServerProxy")
         self.addCleanup(patcher1.stop)
         self.mockServerProxy = patcher1.start()
-        instance = self.mockServerProxy.return_value
-        instance.list_packages.return_value = ["package1", "package2", "package3"]
 
         patcher2 = patch("sys.stdout", new_callable = StringIO.StringIO)
         self.addCleanup(patcher2.stop)
         self.stringIO = patcher2.start()
 
 class ListPackageTest(XmlRpcTest):
+    def setUp(self):
+        super(ListPackageTest, self).setUp()
+        self.list_packages_return_value = ["package1", "package2", "package3"]
+
+        self.mockServerProxy.return_value.list_packages.return_value = self.list_packages_return_value
+
     def test_list(self):
         list_packages("indexUrl1")
         self.mockServerProxy.assert_called_once_with("indexUrl1")
-        self.mockServerProxy.list_packages.assert_called_once()
+        self.mockServerProxy.return_value.list_packages.assert_called_once_with()
 
-        self.assertEquals(self.stringIO.getvalue(), "\n".join(["package1", "package2", "package3"]) + "\n")
+        self.assertEquals(self.stringIO.getvalue(), "\n".join(self.list_packages_return_value) + "\n")
+
+class DocsTest(XmlRpcTest):
+    def setUp(self):
+        super(DocsTest, self).setUp()
+        self.docs_return_value = "Some String"
+
+        self.mockServerProxy.return_value.docs.return_value = self.docs_return_value
+
+    def test_docs(self):
+        docs("indexUrl1", "package1")
+        self.mockServerProxy.assert_called_once_with("indexUrl1")
+        self.mockServerProxy.return_value.docs.assert_called_once_with("package1")
+
+        self.assertEquals(self.stringIO.getvalue(), self.docs_return_value + "\n")
