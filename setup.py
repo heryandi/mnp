f1506a36ab4d9970fb7661aaf2a0f7da05812cec
Heryandi <heryandi@gmail.com>
2013-09-13 14:55:27 +0800

    Add classifier and keyword to metadata

--- setup.py
+++ setup.py
@@ -3,22 +3,26 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name='mnp',
-    version='1.0.0',
-    author='Heryandi',
-    author_email='heryandi@gmail.com',
-    packages=find_packages(exclude='test'),
+    name="mnp",
+    version="1.0.0",
+    author="Heryandi",
+    author_email="heryandi@gmail.com",
+    packages=find_packages(exclude="test"),
     scripts=[],
-    url='https://github.com/heryandi/mnp',
-    license='MIT',
-    description='Wrapper tools to manage Mininet package',
-    long_description=open('README.rst').read(),
+    url="https://github.com/heryandi/mnp",
+    license="MIT",
+    description="Tools to manage Mininet package",
+    long_description=open("README.rst").read(),
     install_requires=[
         "pip",
         "requests",
         "setuptools",
     ],
-    entry_points={'console_scripts': [
-        'mnp = mnp:main',
-    ]}
+    entry_points={"console_scripts": [
+        "mnp = mnp:main",
+    ]},
+    classifiers=[
+        "Mininet :: Tool",
+    ],
+    keywords="command-line commandline mininet package packaging tool"
 )
